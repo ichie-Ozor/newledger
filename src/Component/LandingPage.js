a047785fe3d4cbe9dd40f67c5e96792c500e7fd4
Ichie Ozor <simeon_mc2000@yahoo.com>
2024-02-01 00:00:08 +0100

    updated

--- src/Component/LandingPage.js
+++ src/Component/LandingPage.js
@@ -8,9 +8,10 @@ function LandingPage() {
   const [errorText, setErrorText ] = useState("")
    const auth = useAuth()   //this is imported from the context soas to distribute it to all the components that needs it
   const [ clicked, setClicked ] = useState(true)
+  const [verifyEmail, setVerifyEmail] = useState("")
   const [ isRegister, setIsRegister ] = useState({
     fullName: "",
-    bizName: "",
+    businessName: "",
     email: "",
     password:""
   })
@@ -19,6 +20,8 @@ function LandingPage() {
     password: ""
   }) 
 
+
+  ////////////////Registration
  const onRegister = (e) => {
   e.preventDefault()
   const { name, value } = e.target
@@ -27,6 +30,46 @@ function LandingPage() {
   })
  }
 
+  const isRegiterHandler = (e) => {
+    e.preventDefault()
+    if(isRegister.fullName !== "" && isRegister.businessName !== "" && isRegister.email !== "" && isRegister.password !== ""){
+      setIsRegister({
+        fullName: isRegister.fullName,
+        businessName: isRegister.businessName,
+        email: isRegister.email,
+        password:isRegister.password
+      })
+      console.log(isRegister)    //send this to the backend
+      const baseUrl3 = 'http://localhost:8080/account/signup'
+      axios.post(baseUrl3, isRegister)
+      .then((response) => {
+        if(response.data.status === "Failed"){
+          setVerifyEmail(response.data.message)
+        } else {
+          setVerifyEmail("Please verify your email to continue")
+        }
+      })
+      .catch(error => setErrorText(error))
+
+      setIsRegister({
+        fullName: "",
+        businessName: "",
+        email: "",
+        password:""
+       })
+              
+    } else {
+     setErrorText("Please register Now, Or 😡😡😠😡😡")
+     setIsRegister({
+      fullName: "",
+      businessName: "",
+      email: "",
+      password:""
+     })
+    }
+  }
+
+  //////////////////Signin and fetch signup details from the backend
   const onChange = (e) => {
     e.preventDefault()
     const { name, value } = e.target
@@ -34,48 +77,21 @@ function LandingPage() {
       ...isSigneIn, [name] : value,
     })   
   }
-
-  const isRegiterHandler = (e) => {
-    e.preventDefault()
-    if(isRegister.fullName !== "" && isRegister.bizName !== "" && isRegister.email !== "" && isRegister.password !== ""){
-      setIsRegister({
-        fullName: isRegister.fullName,
-        bizName: isRegister.bizName,
-        email: isRegister.email,
-        password:isRegister.password
-      })
-      console.log(isRegister)
-      setIsRegister({
-        fullName: "",
-        bizName: "",
-        email: "",
-        password:""
-       })
-    } else {
-     setErrorText("Please register Now, Or 😡😡😠😡😡")
-     setIsRegister({
-      fullName: "",
-      bizName: "",
-      email: "",
-      password:""
-     })
-    }
-  }
-
-  //Signin and fetch signup details from the backend
   // const baseUrl = `http://localhost:8080/auth/getaccount/${isSigneIn.email}$${isSigneIn.password}`   //the detail ofthe client with that email//this url is used to get 
   const baseUrl = "http://localhost:8080/auth/signin"
   const isSignInHandler = (e) => {
     e.preventDefault()
     const {email, password} = isSigneIn
+    console.log(isSigneIn)
     /////////////////this sends data to the back for signin 
     axios({
       method: 'post',
       url: baseUrl,
       data: {email, password}
     }).then((response) => {
-    const status = response.request.status
-      if (status === 200){
+    const status = response.data.status
+    console.log(response, status)
+      if (status === "Success"){
           auth.login(email)      //this is supposed to update the state in the context so that it can be available to all the components that needs to extract the details of the user from the backend
           navigate('dashboard')
     } else {
@@ -108,6 +124,7 @@ function LandingPage() {
             <p className='relative left-8'>Enter Your Details Below</p>
           </div>
           <div className='absolute top-48 w-80 left-20 -mb-2 text-red-700 font-bold text-xs md:top-72 md:left-80'>{errorText}</div>
+          <div>{verifyEmail}</div>
           { clicked?
           <>
           <form className='relative p-2 w-96 top-20 md:top-36 md:left-16 ' onSubmit={isSignInHandler}>
@@ -120,7 +137,7 @@ function LandingPage() {
           <>
           <form className='absolute md:relative top-44 md:top-44 p-2 left-16 w-96' onSubmit={isRegiterHandler}>
             <input type='text' placeholder='Full Name' className='input' name='fullName' value={isRegister.fullName} onChange={onRegister}/>
-            <input type='text' placeholder='Business Name' className='input' name='bizName' value={isRegister.bizName} onChange={onRegister}/>
+            <input type='text' placeholder='Business Name' className='input' name='businessName' value={isRegister.businessName} onChange={onRegister}/>
             <input type='email' placeholder='Email Address' className='input' name='email' value={isRegister.email} onChange={onRegister}/>
             <input type='password' placeholder='Password' className='input' name='password' value={isRegister.password} onChange={onRegister}/>
             <button type='submit' className='btn'>Register</button>
